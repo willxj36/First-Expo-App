bc950c55b76bdffc16d6abcd17a32388db5619d2
Will Seese <willxj36@gmail.com>
2021-02-05 22:01:28 -0700

    red text

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sup wit it</Text>
+      <Text style={styles.text}>#crushingit</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#999',
+    backgroundColor: '#60f692',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#f00'
+  }
 });
